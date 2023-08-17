99c188b0f35fc4543833bbce0c4ad7316d718f10
MarcosVini9999 <marcosviniciusandradedesousa@hotmail.com>
2023-08-17 14:25:38 -0300

    feat: github user profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { iUser } from './services/github-user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-github-profile';
+  user: iUser = {} as iUser;
+
+  getUser(user: iUser) {
+    this.user = user;
+    console.log(user);
+  }
 }
--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { iUser } from 'src/app/services/github-user.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent {
-
+  @Input() user: iUser = {} as iUser;
 }
